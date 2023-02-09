990bb2ffb760488672c1ed68988d40fd48800a8d
Mariana <mariana.balanchuk@gmail.com>
2023-02-09 11:57:33 +0100

    added header

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,5 +1,33 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Container } from "@mui/material";
+
 type Props = {};
 const Header = (props: Props) => {
-  return <div>Header</div>;
+  return (
+    <AppBar position="static">
+      <Container>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 };
 export default Header;
